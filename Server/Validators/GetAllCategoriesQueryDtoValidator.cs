5a28d9d0e76b08302d8fe7c40d1409f223c302a3
Andrey <andrey1144@ya.ru>
2022-03-26 23:48:59 +0700

    Validator for GetAllCategoriesQueryDto

---
+++ Server/Validators/GetAllCategoriesQueryDtoValidator.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+using UseCases.Category.Dto;
+
+namespace Server.Validators
+{
+    public class GetAllCategoriesQueryDtoValidator : AbstractValidator<GetAllCategoriesQueryDto>
+    {
+        public GetAllCategoriesQueryDtoValidator()
+        {
+            RuleFor(x => x.Name).MaximumLength(450).When(x => x.Name != null);
+        }
+    }
+}
