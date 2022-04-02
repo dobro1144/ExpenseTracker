df2702dbb4f6bbbc57764cb9a49d1c9c67ae0ece
Andrey <andrey1144@ya.ru>
2022-04-03 00:36:33 +0700

    No cascade delete

--- Entities/Models/Category.cs
+++ Entities/Models/Category.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+
 namespace Entities.Models
 {
     public class Category : Entity
@@ -5,5 +7,6 @@ namespace Entities.Models
         public int UserId { get; set; }
         public User? User { get; set; }
         public string Name { get; set; } = null!;
+        public List<Expense> Expenses { get; set; } = new ();
     }
 }
