2187ef4d18572c8766d465a267d3504abe710309
Andrey <andrey1144@ya.ru>
2022-04-03 00:44:40 +0700

    Migration added

---
+++ DataAccess.MsSql/Migrations/20220402173731_NoCascadeDelete.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccess.MsSql.Migrations
+{
+    public partial class NoCascadeDelete : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_Users_UserId",
+                table: "Accounts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Users_UserId",
+                table: "Categories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Accounts_AccountId",
+                table: "Expenses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Incomes_Accounts_AccountId",
+                table: "Incomes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Expenses",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_Users_UserId",
+                table: "Accounts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Users_UserId",
+                table: "Categories",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Accounts_AccountId",
+                table: "Expenses",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Incomes_Accounts_AccountId",
+                table: "Incomes",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Accounts_Users_UserId",
+                table: "Accounts");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Categories_Users_UserId",
+                table: "Categories");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Expenses_Accounts_AccountId",
+                table: "Expenses");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Incomes_Accounts_AccountId",
+                table: "Incomes");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "CategoryId",
+                table: "Expenses",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Accounts_Users_UserId",
+                table: "Accounts",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Categories_Users_UserId",
+                table: "Categories",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Expenses_Accounts_AccountId",
+                table: "Expenses",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Incomes_Accounts_AccountId",
+                table: "Incomes",
+                column: "AccountId",
+                principalTable: "Accounts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
